dc9b59f62d24f3513df4ca757123a1c0f20e7c80
PonceSantiago <slponce@fi.uba.ar>
2023-05-01 05:31:48 -0300

    Rama Jugador y archivos creados

---
+++ TP2/src/Jugador.hpp
@@ -0,0 +1,25 @@
+#ifndef JUGADOR__H 
+#define JUGADOR__H
+
+#include <iostream>
+
+class Jugador {
+private:
+    int identificador;
+    std::string nombre;
+   
+
+public:
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
