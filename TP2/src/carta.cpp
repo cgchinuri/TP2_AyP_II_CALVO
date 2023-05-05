73dd0d973f2e2c4c037c5316686b8deaa5f2bb1e
facubaratta <fbaratta@fi.uba.ar>
2023-05-05 19:27:44 -0300

    Cambie las cartas

--- TP2/src/carta.cpp
+++ TP2/src/carta.cpp
@@ -13,17 +13,12 @@ Carta::Carta()
 {
     double probabilidad = generarNumeroAleatorio(LAMBDA);
 
-    if (probabilidad < 0.1) this->nombre = SOLDADO_EXTRA;
-    if (probabilidad >= 0.1 && probabilidad < 0.2) this->nombre = AVION;
-    if (probabilidad >= 0.2 && probabilidad < 0.3) this->nombre = TRINCHERA;
-    if (probabilidad >= 0.3 && probabilidad < 0.4) this->nombre = BARCO;
-    if (probabilidad >= 0.4 && probabilidad < 0.5) this->nombre = CONVERSION_SOLDADO;
-    if (probabilidad >= 0.5 && probabilidad < 0.6) this->nombre = ROBO_VEHICULO;
-    if (probabilidad >= 0.6 && probabilidad < 0.7) this->nombre = SUBMARINO;
-    if (probabilidad >= 0.7 && probabilidad < 0.8) this->nombre = ATAQUE_AEREO;
-    if (probabilidad >= 0.8 && probabilidad < 0.9) this->nombre = INVESTIGACION_BOMBA_NUCLEAR;
-    else this->nombre = SOLDADO_EXTRA;
-}
+    if (probabilidad < 0.2) this->nombre = REFUERZOS;
+    if (probabilidad >= 0.2 && probabilidad < 0.4) this->nombre = AVION_RADAR;
+    if (probabilidad >= 0.4 && probabilidad < 0.6) this->nombre = TRINCHERA;
+    if (probabilidad >= 0.6 && probabilidad < 0.8) this->nombre = BARCO;
+    else this->nombre = SUBMARINO;
+
 
 nombreCarta_t Carta::getNombreCarta()
 {
@@ -37,22 +32,16 @@ std::string Carta::cartaToString()
     {
     case SUBMARINO:
         return "Submarino";
-    case ROBO_VEHICULO:
-        return "Robo de Vehiculo";
-    case CONVERSION_SOLDADO:
-        return "Conversion de Soldado Enemigo";
-    case AVION:
-        return "Soporte Aereo";
+    case ATAQUE_QUIMICO:
+        return "Ataque Quimico";
+    case REFUERZOS:
+        return "Refuerzos de Infantería";
+    case AVION_RADAR:
+        return "Radar Aereo Movil";
     case TRINCHERA:
         return "Cavar Trinchera";
     case BARCO:
         return "Barco";
-    case ATAQUE_AEREO:
-        return "Bombardeo Aereo";
-    case INVESTIGACION_BOMBA_NUCLEAR:
-        return "Investigacion Atomica";
-    case SOLDADO_EXTRA:
-        return "Refuerzos de infanteria";
     default:
         return "DESCONOCIDO";
     };
