10a9b6930593e3dbba8414d1602e1c90e96f49cd
Grimmfs <101952122+Grimmfs@users.noreply.github.com>
2023-05-19 14:53:22 -0300

    Update bitmap.cpp

--- src/Utils/bitmap.cpp
+++ src/Utils/bitmap.cpp
@@ -1,1 +1,18 @@
 
+
+enum obtenerTipoCasilla (TDAtablero tablero, int x, int y, int z){
+   return tablero::obtenerCasillero(x,y,z)::getTipoCasillero();
+}
+
+void graficarPlanoZ(TDAtablero tablero, int z, int tamaño){
+  //crea imagen tamañoxtamaño
+   bitmap_image image(tamaño,tamaño);
+  
+   // pone todo los bloques de base en marron(tierra) o celeste (cielo) segun la altura
+  if z<4{
+      image.set_all_channels(115, 64, 32);
+  } else {
+        image.set_all_channels(115, 64, 32);
+
+  for int y=0,y<tamaño,y++{
+    
