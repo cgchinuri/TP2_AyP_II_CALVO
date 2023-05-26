18791ca78b71071232d630e857b9faf3e65c9e00
chris <cchinuri@fi.uba.ar>
2023-05-26 18:32:50 -0300

    Se agrego pre y pos y toStringTipo en Ficha.h

--- src/Ficha/Ficha.hpp
+++ src/Ficha/Ficha.hpp
@@ -16,9 +16,22 @@ private:
     t_ficha tipo;
     bool activo;
 public: 
+
+    //pre: tipo debe ser un valor de t_ficha
+    //post: crea una ficha con el tipo y coordenadas indicadas
     Ficha(t_ficha tipo,int x, int y, int z);
+
+    //pre: -
+    //post: devuelve el tipo de la ficha
     t_ficha obtenerTipo(void)const;
+
+    //pre: -
+    //post: devuelve la coordenada de la ficha
     Coordenada<int> * obtenerCoordenada(void);
+
+    //pre: -
+    //post: devuelve un string con el tipo de la ficha
+    std::string toStringTipo(void);
 };
 
 
