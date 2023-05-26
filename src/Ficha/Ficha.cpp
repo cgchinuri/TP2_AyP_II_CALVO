4e069947f7bd28b955a1e89e173af6527825df7f
chris <cchinuri@fi.uba.ar>
2023-05-26 18:38:41 -0300

    Se agrego validacion  e implemento el metodo toSringTipo en Ficha.cpp

--- src/Ficha/Ficha.cpp
+++ src/Ficha/Ficha.cpp
@@ -4,6 +4,10 @@
 
 
 Ficha::Ficha(t_ficha tipo,int x, int y, int z)  {
+    if(tipo!=FICHA_BARCO && tipo!=FICHA_SOLDADO && tipo!=FICHA_AVION && tipo!=FICHA_MINA){ 
+        throw "Tipo de ficha invalido";
+    } 
+
     this->tipo=tipo;
     this->coordenada=new Coordenada<int>(x,y,z);
     this->activo=true;
@@ -15,4 +19,23 @@ t_ficha Ficha::obtenerTipo(void)const  {
 
 Coordenada<int> * Ficha::obtenerCoordenada(void)  {
     return this->coordenada;
+}
+
+std::string Ficha::toStringTipo(void) {
+    std::string s;
+    switch(this->tipo)  {
+        case FICHA_BARCO:
+            s="Barco";
+            break;
+        case FICHA_SOLDADO:
+            s="Soldado";
+            break;
+        case FICHA_AVION:
+            s="Avion";
+            break;
+        case FICHA_MINA:
+            s="Mina";
+            break;
+    }
+    return s;
 }
